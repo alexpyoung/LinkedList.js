30b64f44513710388bc94e762554c601341f955e
Alex Young <alexyoung1992@gmail.com>
2016-03-05 00:38:33 -0500

    feat(list): define API

--- src/index.js
+++ src/index.js
@@ -1,3 +1,107 @@
 class LinkedList {
 
+    /**
+     * Instantiates LinkedList<Object>
+     * @param {Array<Object>}
+     * @returns {LinkedList<Object>}
+     */
+    constructor(arr) {
+
+    }
+
+    /**
+     * Getter for first elenent
+     * @returns {Node<Object>}
+     */
+    head() {
+
+    }
+
+    /**
+     * Getter for last elenent
+     * @returns {Node<Object>}
+     */
+    tail() {
+
+    }
+
+    /**
+     * Test if the list is empty
+     * @returns {Boolean}
+     */
+    empty() {
+
+    }
+
+    /**
+     * Return size
+     * @returns {Integer}
+     */
+    length() {
+
+    }
+
+    /**
+     * Insert element at beginning
+     * @param {Node<Object>}
+     */
+    pushHead(obj) {
+
+    }
+
+    /**
+     * Remove element from beginning
+     * @returns {Object}
+     */
+    popHead() {
+
+    }
+
+    /**
+     * Insert element at end
+     * @param {Node<Object>}
+     */
+    pushTail(obj) {
+
+    }
+
+    /**
+     * Remove element from end
+     * @returns {Object}
+     */
+    popTail() {
+
+    }
+
+    /**
+     * Removes element that contains the given Object
+     * @param {Object}
+     */
+    remove(obj) {
+
+    }
+
+    /**
+     * Returns element that contains the given Object
+     * @param {Object}
+     * @returns {Node<Object>}
+     */
+    find(obj) {
+
+    }
+
+    /**
+     * Sorts elements
+     * @param {Function}
+     */
+    sort(compare) {
+        
+    }
+
+    /**
+     * Reverses the order of elements
+     */
+    reverse() {
+
+    }
 }
